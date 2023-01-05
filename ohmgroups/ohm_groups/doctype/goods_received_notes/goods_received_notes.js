0239ed506ae1fef8be1d61a861bfef8f0d21674a
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-01-05 19:09:39 +0530

    GRN

--- ohmgroups/ohm_groups/doctype/goods_received_notes/goods_received_notes.js
+++ ohmgroups/ohm_groups/doctype/goods_received_notes/goods_received_notes.js
@@ -1,20 +1,99 @@
 // Copyright (c) 2023, thirvusoft and contributors
 // For license information, please see license.txt
+
 frappe.ui.form.on('Goods Received Notes', {
+	refresh: function(frm) {
+        frm.set_query("party_type",function(){
+            return {
+                filters:{
+                    "name":["in",["Customer","Supplier"]]
+                }
+                
+            }
+        }),
+		frm.set_query("transporter", function() {
+			return {
+				filters: {
+					is_transporter: 1
+				}
+			};
+		});
+		if(!frm.doc.branch){
+            frm.call({
+                
+                method: "grn_address_company",
+                args:{
+                    company:frm.doc.company,
+                },
+    
+                callback: function(r) {
+                    frm.set_value("company_address",r.message)
+            }
+            })
+        }
+	},
+    mode_of_transport(frm) {
+        frm.set_value("gst_vehicle_type", get_vehicle_type(frm.doc));
+    },
+    // branch(frm) {
+    //     frm.call({
+            
+    //         method: "grn_branch_address_company",
+    //         args:{
+    //             company:frm.doc.company,
+    //             branch:frm.doc.branch,
+    //         },
+    //         callback: function(r) {
+    //                 cur_frm.set_value("company_address",r.message)
+                
+    //     }
+    //     })
+    // },
+    
+    party: function(frm){
+        frm.set_value("party_name",frm.doc.party)
+        frm.call({
+            
+            method: "grn_on_insert",
+            args:{
+                party:frm.doc.party,
+            },
+            callback: function(r) {
+                    cur_frm.set_value("warehouse",r.message)
+                
+        }
+        }),
+                      
+        frm.call({
+            
+            method: "grn_address_shipping",
+            args:{
+                party_type:frm.doc.party_type,
+                party : frm.doc.party,
+            },
+
+            callback: function(r) {
+                frm.set_value("shipping_address_name",r.message)
+        }
+        }),
+        frm.call({
+            
+            method: "grn_address_billing",
+            args:{
+                party_type:frm.doc.party_type,
+                party : frm.doc.party,
+            },
+
+            callback: function(r) {
+                frm.set_value("customer_address",r.message)
+        }
+        })
+    
+    },
 
 });
-// frappe.ui.form.on('GRN Items', {
-// 	items: function(frm){
-// 		console.log("----")
-
-// 		frm.call({
-// 			method: "goods_items",
-// 			args:{
-// 				company:frm.doc.company
-// 			},
-// 			callback: function(r){
-// 				console.log(r.message)
-// 			}
-// 		})
-// 	}
-// });
+function get_vehicle_type(doc) {
+    if (doc.mode_of_transport == "Road") return "Regular";
+    if (doc.mode_of_transport == "Ship") return "Over Dimensional Cargo (ODC)";
+    return "";
+}
--- ohmgroups/ohm_groups/doctype/goods_received_notes/goods_received_notes.py
+++ ohmgroups/ohm_groups/doctype/goods_received_notes/goods_received_notes.py
@@ -4,13 +4,60 @@
 import frappe
 from frappe.model.document import Document
 
-# @frappe.whitelist()
-# def goods_items(company,items):
-#     rec_items = frappe.get_all("DC Not for Sales",{'company':company,""},pluck="party_name")
-#     print('Mani')
-#     return rec_items
+@frappe.whitelist()
+def grn_on_insert(party):
+    if party:
+        company = frappe.db.get_single_value("Global Defaults","default_company")
+        abbr = frappe.db.get_value("Company",company,"abbr")
+        if not frappe.db.exists("Warehouse", {'parent_warehouse': f'Supplier Warehouse - {abbr}', 'warehouse_name':party}):
+            document = frappe.new_doc("Warehouse")
+            document.parent_warehouse =f'Supplier Warehouse - {abbr}'
+            document.company = company
+            document.warehouse_name = party
+            document.save(ignore_permissions=True)
+            return document.name
+        else:
+            return frappe.get_value("Warehouse", {'parent_warehouse': f'Supplier Warehouse - {abbr}', 'warehouse_name':party}, pluck="name")
         
+@frappe.whitelist()
+def grn_address_company(company):
+    com_add = frappe.get_value("Dynamic Link", {"parenttype":"Address","link_doctype":"Company","link_name":company},"parent")
+    com_plant_add = frappe.get_value('Address',{'name':com_add})
+    return com_plant_add
 
 
+# @frappe.whitelist()
+# def grn_branch_address_company(company):
+#         # com_add = frappe.get_value("Dynamic Link", {"parenttype":"Address","link_doctype":"Company","link_name":company},"parent")
+#         branch_add = frappe.get_value("Branch",{"company":company,"branch":branch},"address")
+#         return branch_add
+    
+    
+@frappe.whitelist()
+def grn_address_shipping(party_type, party):
+    com_add = frappe.get_all("Dynamic Link", {"parenttype":"Address","link_doctype":party_type,"link_name":party},pluck="parent")
+    for i in com_add:
+        ship_add =  frappe.get_value('Address',{'address_type':'Shipping','name':i},"name")
+        if ship_add:
+            return ship_add
+@frappe.whitelist()
+def grn_address_billing(party_type, party):
+    com_add = frappe.get_all("Dynamic Link", {"parenttype":"Address","link_doctype":party_type,"link_name":party},pluck="parent")
+    for i in com_add:
+        bill_add =  frappe.get_value('Address',{'address_type':'Billing','name':i},"name")
+        if bill_add:
+            return bill_add        
+        
+@frappe.whitelist()
+def grn_dc_items(items,company,party,party_type):
+    get_dc_all = frappe.get_all("DC Not for Sales",{"company":company,"items":items,"party":party,"party_type":party_type})
+    for i in get_dc_all:
+        get_dc_doc = frappe.get_doc("DC Not for Sales",{"name":i.name})
+        for j in get_dc_doc.items:
+            get_dc_items = frappe.get_all("DC Items",{"items":j.item_code})
+     
+     
+     
 class GoodsReceivedNotes(Document):
-    pass
+	pass
+
