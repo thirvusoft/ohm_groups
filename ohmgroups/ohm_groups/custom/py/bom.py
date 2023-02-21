710c59235a5cacb200a52a079cf1ed23834b465f
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-02-21 10:39:22 +0530

    feat: BOM Tree structure Item filters in BOM Raw Material Table

--- ohmgroups/ohm_groups/custom/py/bom.py
+++ ohmgroups/ohm_groups/custom/py/bom.py
@@ -18,7 +18,6 @@ def operations_(item):
     v= item.split('-')
     if v[-1] != "FG":
         fg_item = frappe.get_doc("Item",{"name":item})
-
         if fg_item.has_variants == 1:
             item = frappe.get_all("Item Variant Attribute",filters={"variant_of":["in",item], "attribute_value":["=","Fg"]},fields=["parent","attribute_value"])
             for i in item:
@@ -28,6 +27,7 @@ def operations_(item):
                     # item_name = i.parent
     else:
         items = item 
+        
     variant_of = frappe.get_all("Item",filters = {"name":items}, pluck = "parent_item")
     parent = variant_of
     while(True):
@@ -35,10 +35,12 @@ def operations_(item):
         parent_1 = []
         for j in parent:
             parent_1 += frappe.get_all("Item",filters = {"parent_item":j,"is_group":1}, pluck = "name")
+            
         variant_of += parent_1
         parent = parent_1
         if len(parent_1) == 0 or count > 500:
             break
+    item_code_ = frappe.get_all("Item",filters={"variant_of":["in",variant_of], "attribute_value":["!=","Fg"]},pluck="name") #Get item_code
     attribute_item = frappe.get_all("Item Variant Attribute",filters={"variant_of":["in",variant_of], "attribute_value":["!=","Fg"]},pluck="attribute_value")
     # for i in attribute_item:
         # if not i.attribute_value == "Fg":
@@ -48,6 +50,6 @@ def operations_(item):
         m.append(i.name)
         # for d in range(0,len(oper_),1):
         #     for k in range(d+1,)
-    return items,oper_,m
+    return items,oper_,m,item_code_
     # return items
                
